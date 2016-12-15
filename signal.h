b3163ed085cea7e0e3d63c6b1f9157fe0986603f
Matt Bowyer <mattbowyer@Matts-MacBook-Air-2.local>
2016-12-15 04:20:03 -0500

    implemented proc.h

--- signal.h
+++ signal.h
@@ -6,4 +6,14 @@
 
 typedef void (*sighandler_t)(int);
 
+#define SIGSEGV 14 //--BOW-->>
+#define PROT_NONE 0x000
+#define PROT_READ 0x001
+#define PROT_WRITE 0x003
+typedef struct
+{
+	uint addr;
+	uint type;
+} siginfo_t; //--BOW-->>
+
 #endif
